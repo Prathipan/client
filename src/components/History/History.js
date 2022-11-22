ea99662f4dbdd1f52088f78c53fa9f52a3fe335b
Prathipan <prathipan24p@gmail.com>
2022-11-22 13:46:29 +0530

    final change

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -8,7 +8,8 @@ import { Button } from "@mui/material";
 import axios from "axios";
 import { TransContext } from "../../App";
 
-const api = "https://prathipan-money-manager.herokuapp.com"
+const api = "http://localhost:3003"
+// https://prathipan-money-manager.herokuapp.com
 
 
 const style = {
